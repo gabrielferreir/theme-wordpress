424ca62e82726e05737b7341571ee69553771864
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-02-06 10:13:02 -0200

    Inserindo menu

--- functions.php
+++ functions.php
@@ -1,8 +1,17 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_css');
+add_action('wp_enqueue_scripts', 'theme_css');
 
-function theme_css() {
+function theme_css()
+{
     wp_enqueue_style('css', get_template_directory_uri() . '/css/style.css');
 }
+
+add_action('init', 'twtema_action_init');
+
+function twtema_action_init()
+{
+    register_nav_menu('twmenu-principal', 'Menu principal');
+}
+
 ?>
--- header.php
+++ header.php
@@ -2,22 +2,18 @@
     <h2 class="primary-header__title">Blog</h2>
 
 
-
     <?php get_search_form(); ?>
 
     <nav class="primary-menu">
         <ul class="primary-menu__list">
-            <li><a href="<?= home_url('/'); ?>">Home</a></li>
-            <?php wp_list_pages([
-                'post_type' => 'page',
-                'title_li' => '',
-                'sort_column' => 'post_date',
-                'sort_order' => 'ASC'
-            ]); ?>
-<!--            --><?php //wp_list_categories([
-//                'taxonomy' => 'category',
-//                'title_li' => ''
-//            ]); ?>
+<!--            <li><a href="--><?//= home_url('/'); ?><!--">Home</a></li>-->
+            <?php
+            wp_nav_menu([
+                'theme_location' => 'twmenu-principal',
+                'container' => 'ul',
+                'menu_class' => 'primary-menu__list'
+            ]);
+?>
         </ul>
     </nav>
 </header>
